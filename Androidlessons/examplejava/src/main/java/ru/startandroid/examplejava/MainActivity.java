91242c34512969b0325f940ac78309e5ab718de1
yuliya <yuliyakur2709@gmail.com>
2022-03-19 19:27:40 +0300

    Initial commit

---
+++ Androidlessons/examplejava/src/main/java/ru/startandroid/examplejava/MainActivity.java
@@ -0,0 +1,22 @@
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+/* Это простая программа на Java.
+Присвоить исходному файлу имя "Example.java"
+ */
+class example {
+    // Эта программа начинается с вызова метода main ()
+    public static void main (String args []) {
+        System.out.println("Простая программа на Java.");
+    }
+}
+
+
